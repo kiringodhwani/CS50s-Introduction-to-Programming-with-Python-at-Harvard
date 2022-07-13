3fd418790f815651410aff0d90e18bc14197c012
Kirin Godhwani <109005052+kiringodhwani@users.noreply.github.com>
2022-07-13 19:59:48 -0400

    Create deep.py

---
+++ Problem Set 1/deep.py
@@ -0,0 +1,11 @@
+# Read in answer
+n = input('What is the answer to the great question of life? ')
+
+# Remove whitespace
+n = n.strip()
+
+# Check if equals 42 or if the lowercase version equals forty-two / forty two. We make it lower   # case because we treat the user's answer case insensitively.
+if n == '42' or n.lower() in {'forty-two', 'forty two'}:
+	print('Yes')
+else:
+	print('No')

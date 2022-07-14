46dfafa87fc3a3cb736b25f1f49e557aff8b3e6c
Kirin Godhwani <109005052+kiringodhwani@users.noreply.github.com>
2022-07-13 20:15:50 -0400

    Create bank.py

---
+++ Problem Set 1/bank.py
@@ -0,0 +1,14 @@
+# Read in greeting
+g = input('Enter a greeting: ')
+
+# Strip leading whitespace and make lowercase so that we can treat the user's greeting case      # insensitively.
+g = g.lstrip().lower()
+
+# if statement
+if g.startswith('hello') == True:
+	print('$0')
+# Make sure that it doesn't start with 'Hello'
+elif g.startswith('h') and g.startswith('hello') == False:
+	print('$20')
+else:
+	print('$100')

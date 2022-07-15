a892d162608f05890b08ba6d8c46528f3ccca99d
Kirin Godhwani <109005052+kiringodhwani@users.noreply.github.com>
2022-07-14 20:15:08 -0400

    Update camel.py

--- Problem Set 2: Loops, Lists, Dictionaries/camel.py
+++ Problem Set 2: Loops, Lists, Dictionaries/camel.py
@@ -1,13 +1,14 @@
+# Read in input
 camel_case = input('Enter the name of a variable in camel case: ')
 
-# Define a variable that is initially just equal to the first charin the input string. We know that 
-# this character will be lowercase. We will use said string to build up the string in snake case.
+# Set a variable equal to the first character in the input string. We know that this character will be
+# lowercase since the input is in camel case. We will then use this variable to build the string in snake case
 build = camel_case[0]
 
 # Begin iterating through the remaining characters in the input string skipping the first character
 for c in camel_case[1:]:
 
-# If the current character is in uppercase letter, then append an underscore to the building string 
+# If the current character is an uppercase letter, then append an underscore to the building string 
 # followed by the current character in its lowercase form
   if c.isupper() == True:
     build = build + '_' + c.lower()

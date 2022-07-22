7df1732d3bdd0f7e7eff282c669a2ecc0248455e
Kirin Godhwani <109005052+kiringodhwani@users.noreply.github.com>
2022-07-22 09:26:26 -0400

    Update deep.py

--- Problem Set 1: Conditionals/deep.py
+++ Problem Set 1: Conditionals/deep.py
@@ -4,7 +4,7 @@ n = input('What is the answer to the great question of life? ')
 # Remove whitespace
 n = n.strip()
 
-# Check if equals 42 or if the lowercase version equals forty-two / forty two. We make it lower   # case because we treat the user's answer case insensitively.
+# Check if equals 42 or if the lowercase version equals forty-two / forty two. We make it lowercase because we treat the user's answer case insensitively.
 if n == '42' or n.lower() in {'forty-two', 'forty two'}:
 	print('Yes')
 else:

da2efa79f04abca9bc81a0b8b36fb98b25d94f41
Kirin Godhwani <109005052+kiringodhwani@users.noreply.github.com>
2022-07-22 09:31:32 -0400

    Update bank.py

--- Problem Set 1: Conditionals/bank.py
+++ Problem Set 1: Conditionals/bank.py
@@ -4,10 +4,9 @@ g = input('Enter a greeting: ')
 # Strip leading whitespace and make lowercase so that we can treat the user's greeting case insensitively.
 g = g.lstrip().lower()
 
-# if statement
+# Print 0 if the string starts with “hello”, 20 if it starts with an “h” (but not “hello”), or 100
 if g.startswith('hello') == True:
 	print('$0')
-# Make sure that it doesn't start with 'Hello'
 elif g.startswith('h') and g.startswith('hello') == False:
 	print('$20')
 else:

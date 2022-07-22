48b49ae21a68ff2a72991b82d168db23127f70b5
Kirin Godhwani <109005052+kiringodhwani@users.noreply.github.com>
2022-07-22 09:26:09 -0400

    Update bank.py

--- Problem Set 1: Conditionals/bank.py
+++ Problem Set 1: Conditionals/bank.py
@@ -1,7 +1,7 @@
 # Read in greeting
 g = input('Enter a greeting: ')
 
-# Strip leading whitespace and make lowercase so that we can treat the user's greeting case      # insensitively.
+# Strip leading whitespace and make lowercase so that we can treat the user's greeting case insensitively.
 g = g.lstrip().lower()
 
 # if statement

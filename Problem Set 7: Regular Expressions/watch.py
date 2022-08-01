e7f016df0f8b32b59d8357ce31bcec4006e67ba0
Kirin Godhwani <109005052+kiringodhwani@users.noreply.github.com>
2022-07-31 20:10:31 -0400

    Create watch.py

---
+++ Problem Set 7: Regular Expressions/watch.py
@@ -0,0 +1,22 @@
+import re
+
+
+def main():
+    print(parse(input("HTML: ")))
+
+
+def parse(s):
+    # The goal of this program is to extract the URLs of YouTube videos that are embedded in HTML pages.
+    # These urls are embedded in the src attribute of the pages, so the we must search for a string beginning with 'src='.
+    # The URL, then, must start with 'http' or 'https' followed by '://'.
+    # 'www.' before 'youtube.com/embed/' is optional.
+    # Lastly, capture the string of characters/numbers at the end of the URL (i.e. 'xvFZjo5PgG0' in src='https://www.youtube.com/embed/xvFZjo5PgG0').
+    if matches := re.search(r'src=\"https?:\/\/(?:www\.)?youtube\.com\/embed\/([a-zA-Z0-9]+)', s):
+        shortened = 'https://youtu.be/' + matches.group(1)
+        return shortened
+    else:
+        return None
+
+
+if __name__ == "__main__":
+    main()

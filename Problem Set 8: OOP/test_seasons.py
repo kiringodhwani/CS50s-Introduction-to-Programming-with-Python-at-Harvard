029238f8b4a92e287be1e986bd194093d9730ea9
Kirin Godhwani <109005052+kiringodhwani@users.noreply.github.com>
2022-08-13 02:31:24 -0400

    Update test_seasons.py

--- Problem Set 8: OOP/test_seasons.py
+++ Problem Set 8: OOP/test_seasons.py
@@ -4,7 +4,7 @@ from datetime import date
 
 # Test the user_input function, which validates the user-inputted birthdate.
 def test_user_input():
-    # Test user_input on input that is not formatted in YYYY-MM-DD format.
+    # Test user_input() on input that is not formatted in YYYY-MM-DD format.
     with pytest.raises(SystemExit):
         user_input('February 5, 2002')
     with pytest.raises(SystemExit):
@@ -16,7 +16,7 @@ def test_user_input():
     with pytest.raises(SystemExit):
         user_input('2002-0205')
 
-    # Test user_input on input that is formatted in YYYY-MM-DD format, but has invalid days or months (i.e. a month that is not between 1 and 12).
+    # Test user_input() on input that is formatted in YYYY-MM-DD format, but has invalid days or months (i.e. a month that is not between 1 and 12).
     with pytest.raises(SystemExit):
         user_input('1999-00-25')
     with pytest.raises(SystemExit):
@@ -26,7 +26,7 @@ def test_user_input():
     with pytest.raises(SystemExit):
         user_input('1999-07-32')
 
-# Test the find_difference function, which finds the difference between a birthdate and today's date and then formats the output.
+# Test the find_difference() function, which finds the difference between a birthdate and today's date and then formats the output.
 # Because the output changes every day with today's date, it is difficult to create tests that work everyday.
 def test_find_difference():
     assert find_difference(date.today()) == 'Zero minutes'
